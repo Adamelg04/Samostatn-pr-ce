bc3ace4f87349589457ab269c18bae0e486587d3
Adam Elgheriani <adam.elgheriani.020@g.pslib.cz>
2022-03-21 13:31:20 +0100

    Přidejte soubory projektu.

---
+++ Samostatná práce.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int soucet_S(cislo)
+{
+	int soucetS = 0;
+	for (int i = 0; i <= cislo; i+= 2) {
+		soucetS += i;
+	}
+	return soucetS;
+}
+
+int main() {
+	printf("Soucet sudych cisel je: %d\n", soucet_S(14));
+}
+
